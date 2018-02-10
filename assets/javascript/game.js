4baf60cea020cd7452d6b87adff64b5912c79d4b
SigmaVX <tonywible@gmail.com>
2018-02-09 21:23:12 -0500

    logic updates for hangman JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,14 @@
 
 
 //Create an array of top 100 movies
-var movies = ["Star Wars The Force Awakens", "Avatar", "Titanic", "Jurassic World", "The Avengers", "Star Wars The Last Jedi", "The Dark Knight", "Rogue One A Star Wars Story", "Beauty and the Beast", "Finding Dory", "Star Wars Episode I  The Phantom Menace", "Star Wars", "Avengers Age of Ultron", "The Dark Knight Rises", "Shrek 2", "ET The ExtraTerrestrial", "The Hunger Games Catching Fire", "Pirates of the Caribbean Dead Mans Chest", "The Lion King", "Toy Story 3", "Wonder Woman", "Iron Man 3", "Captain America Civil War", "The Hunger Games", "SpiderMan", "Jurassic Park", "Transformers Revenge of the Fallen", "Frozen", "Guardians of the Galaxy Vol 2", "Harry Potter and the Deathly Hallows Part 2", "Finding Nemo", "Star Wars Episode III  Revenge of the Sith", "The Lord of the Rings The Return of the King", "SpiderMan 2", "The Passion of the Christ", "The Secret Life of Pets", "Despicable Me 2", "The Jungle Book", "Deadpool", "Inside Out", "Jumanji Welcome to the Jungle", "Furious 7", "Transformers Dark of the Moon", "American Sniper", "The Lord of the Rings The Two Towers", "Zootopia", "The Hunger Games Mockingjay  Part 1", "SpiderMan 3", "Minions", "SpiderMan Homecoming", "Alice in Wonderland", "Guardians of the Galaxy", "Batman v Superman Dawn of Justice", "Forrest Gump", "It", "Suicide Squad", "Shrek the Third", "Transformers", "Iron Man", "Harry Potter and the Sorcerers Stone", "Indiana Jones and the Kingdom of the Crystal Skull", "The Lord of the Rings The Fellowship of the Ring", "Thor Ragnarok", "Iron Man 2", "Star Wars Episode II  Attack of the Clones", "Pirates of the Caribbean At Worlds End", "Return of the Jedi", "Independence Day", "Pirates of the Caribbean The Curse of the Black Pearl", "Skyfall", "The Hobbit An Unexpected Journey", "Harry Potter and the HalfBlood Prince", "The Twilight Saga Eclipse", "The Twilight Saga New Moon", "Harry Potter and the Deathly Hallows Part 1", "The Sixth Sense", "Up", "Inception", "The Twilight Saga Breaking Dawn Part 2", "Harry Potter and the Order of the Phoenix", "The Chronicles of Narnia The Lion the Witch and the Wardrobe", "Man of Steel", "The Empire Strikes Back", "Harry Potter and the Goblet of Fire", "Monsters Inc", "Home Alone", "The Hunger Games Mockingjay  Part 2", "The Matrix Reloaded", "The Twilight Saga Breaking Dawn Part 1", "Meet the Fockers", "The Hangover", "Gravity", "Sing", "Monsters University", "Shrek", "Despicable Me 3", "The Amazing SpiderMan", "Harry Potter and the Chamber of Secrets", "The Incredibles", "How the Grinch Stole Christmas"];
+var movies = ["STAR WARS THE FORCE AWAKENS", "AVATAR", "TITANIC", "JURASSIC WORLD", "THE AVENGERS", "STAR WARS THE LAST JEDI", "THE DARK KNIGHT", "ROGUE ONE A STAR WARS STORY", "BEAUTY AND THE BEAST", "FINDING DORY", "STAR WARS EPISODE I  THE PHANTOM MENACE", "STAR WARS", "AVENGERS AGE OF ULTRON", "THE DARK KNIGHT RISES", "SHREK 2", "ET THE EXTRATERRESTRIAL", "THE HUNGER GAMES CATCHING FIRE", "PIRATES OF THE CARIBBEAN DEAD MANS CHEST", "THE LION KING", "TOY STORY 3", "WONDER WOMAN", "IRON MAN 3", "CAPTAIN AMERICA CIVIL WAR", "THE HUNGER GAMES", "SPIDERMAN", "JURASSIC PARK", "TRANSFORMERS REVENGE OF THE FALLEN", "FROZEN", "GUARDIANS OF THE GALAXY VOL 2", "HARRY POTTER AND THE DEATHLY HALLOWS PART 2", "FINDING NEMO", "STAR WARS EPISODE III  REVENGE OF THE SITH", "THE LORD OF THE RINGS THE RETURN OF THE KING", "SPIDERMAN 2", "THE PASSION OF THE CHRIST", "THE SECRET LIFE OF PETS", "DESPICABLE ME 2", "THE JUNGLE BOOK", "DEADPOOL", "INSIDE OUT", "JUMANJI WELCOME TO THE JUNGLE", "FURIOUS 7", "TRANSFORMERS DARK OF THE MOON", "AMERICAN SNIPER", "THE LORD OF THE RINGS THE TWO TOWERS", "ZOOTOPIA", "THE HUNGER GAMES MOCKINGJAY  PART 1", "SPIDERMAN 3", "MINIONS", "SPIDERMAN HOMECOMING", "ALICE IN WONDERLAND", "GUARDIANS OF THE GALAXY", "BATMAN V SUPERMAN DAWN OF JUSTICE", "FORREST GUMP", "IT", "SUICIDE SQUAD", "SHREK THE THIRD", "TRANSFORMERS", "IRON MAN", "HARRY POTTER AND THE SORCERERS STONE", "INDIANA JONES AND THE KINGDOM OF THE CRYSTAL SKULL", "THE LORD OF THE RINGS THE FELLOWSHIP OF THE RING", "THOR RAGNAROK", "IRON MAN 2", "STAR WARS EPISODE II  ATTACK OF THE CLONES", "PIRATES OF THE CARIBBEAN AT WORLDS END", "RETURN OF THE JEDI", "INDEPENDENCE DAY", "PIRATES OF THE CARIBBEAN THE CURSE OF THE BLACK PEARL", "SKYFALL", "THE HOBBIT AN UNEXPECTED JOURNEY", "HARRY POTTER AND THE HALFBLOOD PRINCE", "THE TWILIGHT SAGA ECLIPSE", "THE TWILIGHT SAGA NEW MOON", "HARRY POTTER AND THE DEATHLY HALLOWS PART 1", "THE SIXTH SENSE", "UP", "INCEPTION", "THE TWILIGHT SAGA BREAKING DAWN PART 2", "HARRY POTTER AND THE ORDER OF THE PHOENIX", "THE CHRONICLES OF NARNIA THE LION THE WITCH AND THE WARDROBE", "MAN OF STEEL", "THE EMPIRE STRIKES BACK", "HARRY POTTER AND THE GOBLET OF FIRE", "MONSTERS INC", "HOME ALONE", "THE HUNGER GAMES MOCKINGJAY  PART 2", "THE MATRIX RELOADED", "THE TWILIGHT SAGA BREAKING DAWN PART 1", "MEET THE FOCKERS", "THE HANGOVER", "GRAVITY", "SING", "MONSTERS UNIVERSITY", "SHREK", "DESPICABLE ME 3", "THE AMAZING SPIDERMAN", "HARRY POTTER AND THE CHAMBER OF SECRETS", "THE INCREDIBLES", "HOW THE GRINCH STOLE CHRISTMAS"];
 
+
+// Array For Storing Wrong Gusses
+var wrongLetters = [ ];
+
+// Array For Storing Right Gusses
+var rightLetters = [ ];
 
 
 // Declair & Link Variables From HTML
@@ -13,26 +19,32 @@ var movies = ["Star Wars The Force Awakens", "Avatar", "Titanic", "Jurassic Worl
 // Guesses Remaining On Current Game
  var guessesHTML = document.getElementById("guesses");
 // Wrong Letters Guesses On Current Game
- var lettersHTML = document.getElementById("letters");
+ var wrongLettersHTML = document.getElementById("wrongLetters");
 // The Selected Hangman Word
  var wordHTML = document.getElementById("word"); 
 // Get Started Message
  var getStartedHTML = document.getElementById("startMessage"); 
 // Masked Word
  var maskedHTML = document.getElementById("maskedWord");
+//  System Message
+ var messageHTML = document.getElementById("message"); 
+
+
 
 
 //Start The Game
 function startGame(){    
+     
     // Computer Randomly Picks Word
     var wordPick = movies[Math.floor(Math.random() * 100)];
     console.log(wordPick);
-  
+
     // Add The Word To HTML 
     wordHTML.textContent = wordPick;
 
-    // Masks The Word On Page Using An Empty Array That Is Put Into A String
+    // Masks The Word On Page Using An Empty Array 
     var mask = [];
+    // Loop Through Word To Build Masked Array
     for (var i=0; i < wordPick.length ; i++){
         if (wordPick.charAt(i) === " "){
         mask[i] = wordPick.charAt(i);
@@ -40,22 +52,63 @@ function startGame(){
         else {
         mask[i] = "_";
         }
+        // Puts Masked Array Into A String On HTML
+        var maskString = mask.join("");
+        maskedHTML.textContent = maskString;    
+    } 
+
+    // Log User's Guess
+    document.onkeyup = function(event) {
+        var guess = event.key.toUpperCase();
+        console.log(guess);
+    
+        for (var i=0; i < wordPick.length; i++){
+            // Code If Right
+            if (wordPick.charAt(i) === guess){
+                console.log("letter found");
+                mask[i] = guess;
+                // Puts Masked Array Into A String On HTML
+                var maskString = mask.join("");
+                maskedHTML.textContent = maskString;
+                
+                // Stores Right Letter In Array
+                // Checks If Right Is Already In Array
+                if (rightLetters.indexOf(guess) === -1){
+                    rightLetters.push(guess);
+                    console.log(rightLetters);
+                    }
+                    else {
+                        
+                    }
+            }
+            // Code If Wrong
+            else {
+                // Checks If Wrong Is Already In Array
+                if (wrongLetters.indexOf(guess) === -1){
+                wrongLetters.push(guess);
+                console.log(wrongLetters);
+                }
+                else {
+                    // messageHTML.textContent = "You Already Tried This.";
+                    // console.log(wrongLetters);
+                }
+            
+            } 
+        }
     }
-    var stringStuff = mask.join (" "); 
-    maskedHTML.textContent = mask;
+}
+
+// Add guess to array
+    // document.onkeyup = function(event) {
+    //     userGuessegs.push(event.key.toLowerCase());
+    //     console.log(userGuesses);
+    // }
 
     // Check If User Letter Is A Match
-    // loop through word at each position in array
-    for (var i=0; i < wordPick.length; i++){
-        if (wordPick[1] === "a"){
-            mask[i] = "a";
-        }
-        else {
+    // Loops through word at each position in array
 
-        }
-    }
 
-}
+
 
 // user enters leter 
 // computer checks if letter is accurate
